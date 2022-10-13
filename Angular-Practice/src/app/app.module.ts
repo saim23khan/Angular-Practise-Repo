f9640b0ac5d377c250a26354f7d1d9c2cd7cac8a
Jaffar Abbas <54433038+jaffarabbas@users.noreply.github.com>
2022-10-13 22:20:51 +0500

    Updated src/app/app.module.ts at 13/10/2022 22:20:51

--- Angular-Practice/src/app/app.module.ts
+++ Angular-Practice/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
