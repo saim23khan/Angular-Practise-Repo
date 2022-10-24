d14b641e1f72adf38d74bd631eeab0a37a8bf093
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-24 15:00:14 +0500

    Commited File : book-detials.component.ts at 10/24/2022 3:00:14 PM

---
+++ Angular-Routing/src/app/public/components/book-detials/book-detials.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-detials',
+  templateUrl: './book-detials.component.html',
+  styleUrls: ['./book-detials.component.css']
+})
+export class BookDetialsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
