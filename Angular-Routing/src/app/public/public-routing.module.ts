d70db344ae7130bbef2742d104549f41067d60dc
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-24 17:52:14 +0500

    Commited File : public-routing.module.ts at 10/24/2022 5:52:14 PM

--- Angular-Routing/src/app/public/public-routing.module.ts
+++ Angular-Routing/src/app/public/public-routing.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { PublicComponent } from './public.component';
+import { AllBooksComponent } from './components/all-books/all-books.component';
+import { BookDetialsComponent } from './components/book-detials/book-detials.component';
 
 
 const routes: Routes = [
-  {path:'auth',component: PublicComponent,children:[
-    // {path:'login',component: LoginComponent},
-    // {path:'signup',component: SignupComponent},
+  {path:'public',component: PublicComponent,children:[
+    {path:'all-books',component: AllBooksComponent},
+    {path:'book-details/:id',component: BookDetialsComponent},
   ]}
 ];
 
 @NgModule({
-  declarations: [],
   imports: [
     RouterModule.forRoot(routes)
   ],
