8cf5b351f2493f8a7374477c4ca422b47816dad0
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-25 00:19:46 +0500

    Commited File : app-routing.module.ts at 10/25/2022 12:19:46 AM

--- Angular-Routing/src/app/app-routing.module.ts
+++ Angular-Routing/src/app/app-routing.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { AuthComponent } from './auth/auth.component';
+import { PublicComponent } from './public/public.component';
 
 const routes: Routes = [
-  {path:'about', component: AboutComponent},
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  //lazy loading
+  { path: 'auth', component: AuthComponent, loadChildren: () => import('./auth/auth.module').then(x => x.AuthModule) },
+  { path: 'public', component: PublicComponent, loadChildren: () => import('./public/public.module').then(a => a.PublicModule) },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
