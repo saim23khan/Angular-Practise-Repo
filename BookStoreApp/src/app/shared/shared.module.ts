562aa3dff2798f4783f98878c00eb9ed2ae00de1
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-27 00:17:13 +0500

    Commited File : shared.module.ts at 10/27/2022 12:17:13 AM

--- BookStoreApp/src/app/shared/shared.module.ts
+++ BookStoreApp/src/app/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MaterialModule } from './material.module';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NotFoundComponent,
+    ToolbarComponent,
+  ],
   imports: [
-    CommonModule
+    MaterialModule,
+  ],
+  exports:[
+    ToolbarComponent
   ]
 })
 export class SharedModule { }
