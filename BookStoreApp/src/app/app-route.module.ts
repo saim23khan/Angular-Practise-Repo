7a5f9e62784af9e5fe25c3217c1f18c583e3e178
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-27 00:44:39 +0500

    Commited File : app-route.module.ts at 10/27/2022 12:44:39 AM

--- BookStoreApp/src/app/app-route.module.ts
+++ BookStoreApp/src/app/app-route.module.ts
@@ -4,12 +4,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
 import { AboutComponent } from './public/about/about.component';
 import { HowItWorksComponent } from './public/how-it-works/how-it-works.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { AuthComponent } from './auth/auth.component';
+import { BooksComponent } from './books/books.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'how-it-works', component: HowItWorksComponent },
+  { path: 'auth', component: AuthComponent, loadChildren: () => import('./auth/auth.module').then(x => x.AuthModule) },
+  { path: 'books', component: BooksComponent, loadChildren: () => import('./books/books.module').then(a => a.BooksModule) },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
