6339c78d215b565d3d98dc49d7422fe4dfb4cad3
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-27 00:44:41 +0500

    Commited File : books-routing.module.ts at 10/27/2022 12:44:41 AM

--- BookStoreApp/src/app/books/books-routing.module.ts
+++ BookStoreApp/src/app/books/books-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AllBooksComponent } from './components/all-books/all-books.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'all-books', pathMatch: 'full' },
+  { path: 'all-books', component: AllBooksComponent },
+  { path: 'book-details/:id', component: BookDetailsComponent },
+];
 
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule],
 })
 export class BooksRoutingModule { }
