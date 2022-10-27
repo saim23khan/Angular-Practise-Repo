a9cd15acbf95cf8b63febf06d2cea9b676524674
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-27 23:11:14 +0500

    Commited File : material.module.ts at 10/27/2022 11:11:14 PM

--- BookStoreApp/src/app/shared/material.module.ts
+++ BookStoreApp/src/app/shared/material.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
   exports: [
+    MatCardModule,
     MatIconModule,
     MatToolbarModule,
     MatButtonModule
