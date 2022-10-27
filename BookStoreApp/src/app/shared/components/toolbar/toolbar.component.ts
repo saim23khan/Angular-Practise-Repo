6fb6d2801771b794fb543cfeec4444fc3b2d63df
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-27 23:11:14 +0500

    Commited File : toolbar.component.ts at 10/27/2022 11:11:14 PM

--- BookStoreApp/src/app/shared/components/toolbar/toolbar.component.ts
+++ BookStoreApp/src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
-
+  //navigation functions
+  public navigater(url:string):void{
+    this.router.navigate([url]);
+  }
 }
