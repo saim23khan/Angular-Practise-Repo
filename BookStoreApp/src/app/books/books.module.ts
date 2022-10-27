9f91e423bea5fc7b9ae6bff215967db6fab4591e
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-27 21:19:30 +0500

    Commited File : books.module.ts at 10/27/2022 9:19:30 PM

--- BookStoreApp/src/app/books/books.module.ts
+++ BookStoreApp/src/app/books/books.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BooksComponent } from './books.component';
 import { BooksRoutingModule } from './books-routing.module';
+import { AllBooksComponent } from './components/all-books/all-books.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
 
 
 
 @NgModule({
   declarations: [
-    BooksComponent
+    BooksComponent,
+    AllBooksComponent,
+    BookDetailsComponent
   ],
   imports: [
-    CommonModule,
     BooksRoutingModule
   ]
 })
