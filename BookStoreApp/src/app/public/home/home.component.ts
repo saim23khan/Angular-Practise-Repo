dca509fa279e82900974ee21a96035a63311a3ff
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-28 14:41:04 +0500

    Commited File : home.component.ts at 10/28/2022 2:41:03 PM

--- BookStoreApp/src/app/public/home/home.component.ts
+++ BookStoreApp/src/app/public/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AuthComponent } from 'src/app/auth/auth.component';
+import { AutherComponent } from 'src/app/shared/components/auther/auther.component';
+import { DataService } from 'src/app/shared/services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('authers') private childAuther!: AutherComponent;
+  @ViewChild('title') private titleChange!: ElementRef;
+  
+  constructor(public _dataService: DataService) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    setTimeout(()=>{
+      this.childAuther.setData(3);
+      this.titleChange.nativeElement.innerHTML = 'hlell'
+    },0);
+  }
+
+  childData($event: any): void {
+    console.log($event)
+  }
 }
