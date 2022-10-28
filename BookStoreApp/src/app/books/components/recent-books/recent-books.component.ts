a6d5740e93a0f2f4131a3431af9bbe8ee02986ea
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-28 14:40:49 +0500

    Commited File : recent-books.component.ts at 10/28/2022 2:40:49 PM

---
+++ BookStoreApp/src/app/books/components/recent-books/recent-books.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-books',
+  templateUrl: './recent-books.component.html',
+  styleUrls: ['./recent-books.component.scss']
+})
+export class RecentBooksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
