c6b77077913bb6e237c724bce22f391abf3d87ee
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-28 14:40:56 +0500

    Commited File : app.module.ts at 10/28/2022 2:40:56 PM

--- BookStoreApp/src/app/app.module.ts
+++ BookStoreApp/src/app/app.module.ts
@@ -9,19 +9,29 @@ import { ToolbarComponent } from './shared/components/toolbar/toolbar.component'
 import { AuthModule } from './auth/auth.module';
 import { BooksModule } from './books/books.module';
 import { CommonModule } from '@angular/common';
+import { BooksComponent } from './books/books.component';
+import { HomeComponent } from './public/home/home.component';
+import { AboutComponent } from './public/about/about.component';
+import { HowItWorksComponent } from './public/how-it-works/how-it-works.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HowItWorksComponent
   ],
   imports: [
+    SharedModule,
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
-    SharedModule,
     AuthModule,
     BooksModule,
     AppRouteModule,
+  ],
+  exports:[
+    // HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
