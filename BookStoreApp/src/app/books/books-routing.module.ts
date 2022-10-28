bd7477fcf331b9274aa7fa35828e03b79ac20ac8
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-28 20:41:46 +0500

    Commited File : books-routing.module.ts at 10/28/2022 8:41:46 PM

--- BookStoreApp/src/app/books/books-routing.module.ts
+++ BookStoreApp/src/app/books/books-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AllBooksComponent } from './components/all-books/all-books.component';
 import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { AddBooksComponent } from './components/add-books/add-books.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: 'all-books', pathMatch: 'full' },
   { path: 'books', component: AllBooksComponent },
+  { path: 'new', component: AddBooksComponent },
   // { path: 'all-books', component: AllBooksComponent },
   { path: 'book-details/:id', component: BookDetailsComponent },
 ];
