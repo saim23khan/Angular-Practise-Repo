bde7e1d7bf3bd342c3fbc2adb246c4c54de4b5e6
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-29 13:49:38 +0500

    Commited File : books.service.ts at 10/29/2022 1:49:38 PM

--- BookStoreApp/src/app/books/service/books.service.ts
+++ BookStoreApp/src/app/books/service/books.service.ts
@@ -6,257 +6,64 @@ import { BooksModel } from '../model/book.model';
 })
 export class BooksService {
 
+  private books = [
+    {
+      "id": 1,
+      "title": "asdasdasd",
+      "totalPages": 100,
+      "auther": "J.k",
+      "price": {
+        "currency": "RS",
+        "value": 100
+      }
+    },
+    {
+      "id": 2,
+      "title": "asdasdasd",
+      "totalPages": 100,
+      "auther": "J.k",
+      "price": {
+        "currency": "RS",
+        "value": 100
+      }
+    },
+    {
+      "id": 3,
+      "title": "asdasdasd",
+      "totalPages": 100,
+      "auther": "J.k",
+      "price": {
+        "currency": "RS",
+        "value": 100
+      }
+    },
+    {
+      "id": 4,
+      "title": "asdasdasd",
+      "totalPages": 100,
+      "auther": "J.k",
+      "price": {
+        "currency": "RS",
+        "value": 100
+      }
+    },{
+      "id": 5,
+      "title": "asdasdasd",
+      "totalPages": 100,
+      "auther": "J.k",
+      "price": {
+        "currency": "RS",
+        "value": 100
+      }
+    }
+  ];
   constructor() { }
 
+  public addBooks(bookModel: BooksModel):void{
+    this.books.push(bookModel);
+    console.log(bookModel);
+  }
   public getBooks(): BooksModel[] {
-    return [
-      {
-        "id": 1,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },
-      {
-        "id": 2,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },
-      {
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },
-      {
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },{
-        "id": 3,
-        "title": "asdasdasd",
-        "totalPages": 100,
-        "auther": "J.k",
-        "price": {
-          "currency": "RS",
-          "value": 100
-        }
-      },
-    ];
+    return this.books
   }
 }
