1180d34a4bb61277bfd3c0f71cd084350aa08ceb
Jaffar Abbas <gamaportal8@gmail.com>
2022-10-29 13:49:37 +0500

    Commited File : add-books.component.ts at 10/29/2022 1:49:36 PM

--- BookStoreApp/src/app/books/components/add-books/add-books.component.ts
+++ BookStoreApp/src/app/books/components/add-books/add-books.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { NavigaterService } from 'src/app/shared/services/navigater.service';
+import { BooksModel } from '../../model/book.model';
+import { BooksService } from '../../service/books.service';
 
 @Component({
   selector: 'app-add-books',
   templateUrl: './add-books.component.html',
   styleUrls: ['./add-books.component.scss']
 })
-export class AddBooksComponent implements OnInit {
+export class AddBooksComponent implements OnInit,AfterViewChecked {
 
-  constructor(public _navigationService: NavigaterService) { }
+  @ViewChild('myForm') myForms!: ElementRef;
+  
+  constructor(public _navigationService: Router,private bookService: BooksService) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewChecked(): void {
+    
+  }
+
+  onSubmitForm(value:any): void{
+    const book: BooksModel = new BooksModel();
+    book.title = value.title;
+    book.auther = value.auther;
+    book.totalPages = value.pages;
+    book.price = {
+      currency: value.currency,
+      value: value.price,
+    }
+    this.bookService.addBooks(book);
+  }
 }
+
