640328537277a512980a38b94aa4f4b65c1c6fcb
jaffarabbas <gamaportal8@gmail.com>
2022-11-01 15:10:42 +0500

    Updated src/app/app-routing.module.ts at 01/11/2022 15:10:42

--- Angular-Route-Guard/src/app/app-routing.module.ts
+++ Angular-Route-Guard/src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './add/add.component';
 import { ContactComponent } from './contact/contact.component';
 import { AuthGuard } from './guard/auth.guard';
 import { RoleGuard } from './guard/role.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
+import { ViewComponent } from './view/view.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent,canActivate:[AuthGuard]},
   {path:'contact',component:ContactComponent,canActivate:[RoleGuard]},
-  {path:'user',component:UserComponent,canActivate:[RoleGuard]},
+  {path:'user',component:UserComponent,children:[
+    {path:'view',component:ViewComponent},
+    {path:'add',component:AddComponent},
+  ],canActivate:[RoleGuard]},
   {path:'login',component:LoginComponent},
 ];
 
