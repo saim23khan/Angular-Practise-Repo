dff171e1af055b0dc3c628e6727a9252f753a63a
jaffarabbas <gamaportal8@gmail.com>
2022-11-02 18:23:37 +0500

    Updated src/app/popup/popup.component.ts at 02/11/2022 18:23:37

--- Angular-Reactive-Forms/src/app/popup/popup.component.ts
+++ Angular-Reactive-Forms/src/app/popup/popup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-popup',
@@ -6,10 +9,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popup.component.scss']
 })
 export class PopupComponent implements OnInit {
+	closeResult = '';
+  errorMessage = "";
+  erorClass = "";
 
-  constructor() { }
-
+	constructor(private modalService: NgbModal) {}
   ngOnInit(): void {
+   
+  }
+  //reactive form
+  formGroup = new FormGroup({
+    code:new FormControl({
+      value:'',
+      disabled:true
+    }),
+    name:new FormControl('',Validators.compose([
+      Validators.required,
+      Validators.minLength(5)
+    ])),
+    email:new FormControl('',Validators.compose([
+      Validators.required,
+      Validators.email
+    ])),
+    phone:new FormControl(),
+    designation:new FormControl(),
+  });
+
+  saveEmployee() {
+    if(this.formGroup.valid){
+
+    }else{
+      this.errorMessage = "Please Enter Valid Data!!";
+      this.erorClass = "errormessage";
+    }
   }
 
+	open(content: any) {
+		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+			(result) => {
+				this.closeResult = `Closed with: ${result}`;
+			},
+			(reason) => {
+				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+			},
+		);
+	}
+
+	private getDismissReason(reason: any): string {
+		if (reason === ModalDismissReasons.ESC) {
+			return 'by pressing ESC';
+		} else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+			return 'by clicking on a backdrop';
+		} else {
+			return `with: ${reason}`;
+		}
+	}
+
+  get name(){
+    return this.formGroup.get("name");
+  }
 }
