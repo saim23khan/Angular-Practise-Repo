46570e20f386e4f3b3501efa00c846b7d2f48180
jaffarabbas <gamaportal8@gmail.com>
2022-11-25 16:46:02 +0500

    Inserted src/app/components/main-container/chat-area/chat-default-page/chat-default-page.component.ts at 25/11/2022 16:46:02

---
+++ Angular-WhatsApp-Clone/src/app/components/main-container/chat-area/chat-default-page/chat-default-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-default-page',
+  templateUrl: './chat-default-page.component.html',
+  styleUrls: ['./chat-default-page.component.scss']
+})
+export class ChatDefaultPageComponent {
+
+}
