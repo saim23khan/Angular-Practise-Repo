813b975df699dbfc1811cabca492d2c5422fccd9
jaffarabbas <gamaportal8@gmail.com>
2022-11-25 16:46:02 +0500

    Inserted src/app/components/main-container/chat-area/chat-default-page/chat-default-page.component.spec.ts at 25/11/2022 16:46:02

---
+++ Angular-WhatsApp-Clone/src/app/components/main-container/chat-area/chat-default-page/chat-default-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChatDefaultPageComponent } from './chat-default-page.component';
+
+describe('ChatDefaultPageComponent', () => {
+  let component: ChatDefaultPageComponent;
+  let fixture: ComponentFixture<ChatDefaultPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ChatDefaultPageComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ChatDefaultPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
