0bd91f04accf37e8971e0e9d941873e904a51233
jaffarabbas <gamaportal8@gmail.com>
2022-11-25 16:46:02 +0500

    Inserted src/app/app.module.ts at 25/11/2022 16:46:02

---
+++ Angular-WhatsApp-Clone/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainContainerComponent } from './components/main-container/main-container.component';
+import { SidebarComponent } from './components/main-container/sidebar/sidebar.component';
+import { SidebarContentComponent } from './components/main-container/sidebar/sidebar-content/sidebar-content.component';
+import { ChatAreaComponent } from './components/main-container/chat-area/chat-area.component';
+import { ChatDefaultPageComponent } from './components/main-container/chat-area/chat-default-page/chat-default-page.component';
+import { ChatRoomComponent } from './components/main-container/chat-area/chat-room/chat-room.component';
+import { MaterialModule } from './shared/material.module';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainContainerComponent,
+    SidebarComponent,
+    SidebarContentComponent,
+    ChatAreaComponent,
+    ChatDefaultPageComponent,
+    ChatRoomComponent,
+    LoginComponent
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MaterialModule,
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
