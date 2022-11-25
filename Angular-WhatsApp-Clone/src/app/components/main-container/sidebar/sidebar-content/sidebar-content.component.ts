44ce8d4b28406cc2427a032b6b558bbae08d264b
jaffarabbas <gamaportal8@gmail.com>
2022-11-25 16:46:03 +0500

    Inserted src/app/components/main-container/sidebar/sidebar-content/sidebar-content.component.ts at 25/11/2022 16:46:03

---
+++ Angular-WhatsApp-Clone/src/app/components/main-container/sidebar/sidebar-content/sidebar-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-content',
+  templateUrl: './sidebar-content.component.html',
+  styleUrls: ['./sidebar-content.component.scss']
+})
+export class SidebarContentComponent {
+
+}
