33d9bc65ec3030012a3088642e601584fbc06c82
jaffarabbas <gamaportal8@gmail.com>
2022-11-26 17:14:01 +0500

    Updated src/app/shared/material.module.ts at 26/11/2022 17:14:01

--- Angular-WhatsApp-Clone/src/app/shared/material.module.ts
+++ Angular-WhatsApp-Clone/src/app/shared/material.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { MatIconModule} from '@angular/material/icon';
 import { MatMenuModule } from "@angular/material/menu";
 import {MatButtonModule} from "@angular/material/button";
+import { asapScheduler, sample } from 'rxjs';
+import { DataSource } from '@angular/cdk/collections';
+import { sanitizeIdentifier } from '@angular/compiler';
+import { NG_ASYNC_VALIDATORS } from '@angular/forms';
+import { HttpHandler } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
 
 const modules:any[] = [
   MatButtonModule,
   MatIconModule,
   MatMenuModule,
+  MatCardModule
 ];
 
 @NgModule({
@@ -18,4 +25,5 @@ const modules:any[] = [
     ...modules
   ]
 })
+
 export class MaterialModule { }
