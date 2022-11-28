8822e1753932f7e77098c1075382ced2b8998944
jaffarabbas <gamaportal8@gmail.com>
2022-11-28 15:31:52 +0500

    Inserted src/app/app-routing.module.ts at 28/11/2022 15:31:52

---
+++ Angular-Firebase-Auithentication/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
+
+const routes: Routes = [
+  {
+    path:'',
+    component: DashboardComponent,
+  },
+  {
+    path:'login/',
+    component: LoginComponent
+  },
+  {
+    path:'register/',
+    component: RegisterComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
