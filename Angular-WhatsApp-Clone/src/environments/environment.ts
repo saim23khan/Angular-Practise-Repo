0b6b7f0dc924facc5b3cf8d7786a8153f7d497a7
jaffarabbas <gamaportal8@gmail.com>
2022-11-28 15:04:08 +0500

    Inserted src/environments/environment.ts at 28/11/2022 15:04:08

---
+++ Angular-WhatsApp-Clone/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCMvJg_wk7Kp4GQVr_dTR9y6z-pMa60tjs",
+    authDomain: "angular-whatsapp-clone-1399.firebaseapp.com",
+    projectId: "angular-whatsapp-clone-1399",
+    storageBucket: "angular-whatsapp-clone-1399.appspot.com",
+    messagingSenderId: "937253371067",
+    appId: "1:937253371067:web:cca7073b3d71454e5f7d9e"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
