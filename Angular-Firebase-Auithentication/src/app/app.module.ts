ab7f331918220effbdca9491a885a8e1ba10a972
jaffarabbas <gamaportal8@gmail.com>
2022-11-28 15:31:52 +0500

    Updated src/app/app.module.ts at 28/11/2022 15:31:52

--- Angular-Firebase-Auithentication/src/app/app.module.ts
+++ Angular-Firebase-Auithentication/src/app/app.module.ts
@@ -5,16 +5,27 @@ import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import {environment} from "../environments/environment";
 import {getAuth, provideAuth} from "@angular/fire/auth";
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
-    provideAuth(()=>getAuth())
+    provideAuth(() => getAuth()),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
