3bcb21a47bea09d1cd288ee70cba692b4ce28e11
jaffarabbas <gamaportal8@gmail.com>
2022-11-28 15:04:08 +0500

    Inserted src/environments/environment.prod.ts at 28/11/2022 15:04:08

---
+++ Angular-WhatsApp-Clone/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCMvJg_wk7Kp4GQVr_dTR9y6z-pMa60tjs",
+    authDomain: "angular-whatsapp-clone-1399.firebaseapp.com",
+    projectId: "angular-whatsapp-clone-1399",
+    storageBucket: "angular-whatsapp-clone-1399.appspot.com",
+    messagingSenderId: "937253371067",
+    appId: "1:937253371067:web:cca7073b3d71454e5f7d9e"
+  }
+};
