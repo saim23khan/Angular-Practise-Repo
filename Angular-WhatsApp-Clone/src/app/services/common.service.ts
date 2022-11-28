dd14e208e100c25e20c31b2bb3ad80de6e7230bb
jaffarabbas <gamaportal8@gmail.com>
2022-11-28 15:04:08 +0500

    Updated src/app/services/common.service.ts at 28/11/2022 15:04:08

--- Angular-WhatsApp-Clone/src/app/services/common.service.ts
+++ Angular-WhatsApp-Clone/src/app/services/common.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {AngularFirestore, AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {BehaviorSubject, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonService {
+  private pathParamState = new BehaviorSubject<string>('');
+  pathParam: Observable<string>
 
-  constructor() { }
+  constructor(private angularFireStore: AngularFirestore,
+              private auth: AngularFireAuth
+              ) {
+    this.pathParam = this.pathParamState.asObservable();
+  }
 }
