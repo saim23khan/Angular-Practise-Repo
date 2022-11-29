e29ebdf9909b0be1b1af3ab77cd3f812938533e1
jaffarabbas <gamaportal8@gmail.com>
2022-11-29 17:40:59 +0500

    Inserted src/app/services/common.service.ts at 29/11/2022 17:40:59

---
+++ Angular-Firebase-Auithentication/src/app/services/common.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import {
+  Auth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  getAuth,
+  authState, onAuthStateChanged
+} from "@angular/fire/auth";
+import firebase from "firebase/compat/app";
+import {FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+  private _user = this.auth.currentUser;
+  constructor(private auth:Auth,private router:Router) {
+    onAuthStateChanged(auth,(user)=>{
+      if(user){
+        this._user = user;
+        console.log(this._user);
+        // localStorage.setItem('user',JSON.stringify(this._user));
+        this.router.navigateByUrl('/login').then();
+      }else{
+        localStorage.setItem('user','');
+      }
+    })
+  }
+
+  public loginWithEmail(formGroup:FormGroup):void{
+    signInWithEmailAndPassword(
+      this.auth,
+      formGroup.value.email,
+      formGroup.value.password)
+      .then((value:any)=>{
+        alert('done');
+        this.router.navigate(['']);
+        }).catch((error:any) => {
+        alert(error);
+      });
+  }
+
+  public google():void{
+    signInWithPopup(this.auth,new GoogleAuthProvider()).then(result=>{
+      console.log(result.user);
+      alert('SignIn With Google');
+      this.router.navigateByUrl('').then();
+    }).catch(error=>{
+      alert(error);
+    });
+  }
+
+  public logout():void{
+    this.auth.signOut().then(()=>{
+      alert('SignOut');
+      this.router.navigateByUrl('/login').then();
+    }).catch(error=>{
+      alert(error);
+    })
+  }
+
+}
