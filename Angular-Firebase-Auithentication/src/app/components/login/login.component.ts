bf5db80d188e9ab587cc08adeaef336896ad4cc5
jaffarabbas <gamaportal8@gmail.com>
2022-11-29 17:41:00 +0500

    Updated src/app/components/login/login.component.ts at 29/11/2022 17:41:00

--- Angular-Firebase-Auithentication/src/app/components/login/login.component.ts
+++ Angular-Firebase-Auithentication/src/app/components/login/login.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, NgForm, Validators} from "@angular/forms";
 
+import {Router} from "@angular/router";
+import {CommonService} from "../../services/common.service";
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  constructor(private firebaseService:CommonService,private router:Router) {
+  }
   formGroup = new FormGroup({
     email:new FormControl(),
     password:new FormControl(),
   });
   loginEmployee():void{
     console.log(this.formGroup.value)
+    this.firebaseService.loginWithEmail(this.formGroup);
   }
+
+  googleLogin():void{
+    this.firebaseService.google();
+  }
+
 }
