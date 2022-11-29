911d487471dbcf275eb1d239d1d65a4931de9880
jaffarabbas <gamaportal8@gmail.com>
2022-11-29 17:41:01 +0500

    Updated src/app/components/login/login.component.ts at 29/11/2022 17:41:01

--- Angular-WhatsApp-Clone/src/app/components/login/login.component.ts
+++ Angular-WhatsApp-Clone/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CommonService} from "../../services/common.service";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  // constructor(private firebaseService:CommonService) {
+  // }
 
+  login():void{
+    // this.firebaseService.loginWithGoogle();
+  }
 }
