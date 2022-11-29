19f3ea55f71e06ee5bff17af02ac424a156938a9
jaffarabbas <gamaportal8@gmail.com>
2022-11-29 17:41:00 +0500

    Updated src/app/components/register/register.component.ts at 29/11/2022 17:41:00

--- Angular-Firebase-Auithentication/src/app/components/register/register.component.ts
+++ Angular-Firebase-Auithentication/src/app/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
+import {Auth,createUserWithEmailAndPassword} from "@angular/fire/auth";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -7,11 +9,27 @@ import {FormControl, FormGroup} from "@angular/forms";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  constructor(private auth:Auth,private router:Router) {
+  }
+
   formGroup = new FormGroup({
     email:new FormControl(),
     password:new FormControl(),
   });
   saveEmployee():void{
     console.log(this.formGroup.value)
+    createUserWithEmailAndPassword(
+      this.auth,
+      this.formGroup.value.email,
+      this.formGroup.value.password)
+      .then((value:any)=>{
+        console.log(value);
+        alert('Successfully Registered');
+        this.formGroup.reset();
+        this.router.navigate(['login']);
+      }).catch((error:any) => {
+        alert(error);
+      });
   }
 }
