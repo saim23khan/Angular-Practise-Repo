3acc876ccfd4a150a797e7fc5dddb9e885a6c451
jaffarabbas <gamaportal8@gmail.com>
2022-11-29 17:41:00 +0500

    Updated src/app/components/dashboard/dashboard.component.ts at 29/11/2022 17:41:00

--- Angular-Firebase-Auithentication/src/app/components/dashboard/dashboard.component.ts
+++ Angular-Firebase-Auithentication/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CommonService} from "../../services/common.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-
+  constructor(private firebaseService:CommonService) {
+  }
+  logOut():void{
+    this.firebaseService.logout()
+  }
 }
