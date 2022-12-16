96017efc39908c37ec2979898d92b3d482cd7d81
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-16 16:20:32 +0500

    Commited File : app.module.ts at 12/16/2022 4:20:32 PM

---
+++ Angular-JWT-Authentication/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { AuthGuard } from './Guard/auth.guard';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    AuthGuard
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
