8375e66ca3ff3ba1bb9adb88da71af3dbc333985
Jaffar Abbas <gamaportal8@gmail.com>
2023-01-18 08:34:55 +0500

    1-18-2023

---
+++ Angular-Reactive-Forms/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'Angular-Reactive-Forms',
+  webDir: 'dist/angular-reactive-forms',
+  bundledWebRuntime: false
+};
+
+export default config;
