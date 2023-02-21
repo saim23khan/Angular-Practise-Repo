144c4d326aac2ed50265fcabb9d938e74d32d0ff
Jaffar Abbas <gamaportal8@gmail.com>
2023-02-21 19:41:25 +0500

    2-21-2023

--- Angular-Dynamic-Header-With-Url/src/app/components/header/header.component.ts
+++ Angular-Dynamic-Header-With-Url/src/app/components/header/header.component.ts
@@ -9,18 +9,27 @@ import {HeaderServiceService} from "../../service/header-service.service";
 export class HeaderComponent implements OnInit{
   showFlag = true;
 
-  constructor(private router: Router, private headerService: HeaderServiceService) {}
+  constructor(private router: Router, private headerService: HeaderServiceService) {
+
+  }
 
   ngOnInit() {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        console.log(event.url);
-        console.log(this.showFlag);
-        // update the showFlag property based on the current URL
-        this.showFlag = this.headerService.shouldShowFlag(event.url);
-      }else{
-        console.log('not navigation end');
-      }
-    });
+    this.showFlag=this.headerService.checkUrl();
+    // this.router.events.subscribe((event) => {
+    //   if (event instanceof NavigationEnd) {
+    //     console.log(event.url);
+    //     console.log(this.showFlag);
+    //     // update the showFlag property based on the current URL
+    //     this.showFlag = this.headerService.shouldShowFlag(event.url);
+    //   }else{
+    //     console.log('not navigation end');
+    //   }
+    // });
+    // if (this.router.url.startsWith('/', 4)) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+  // }
   }
 }
--- Angular-Dynamic-Header-With-Url/src/app/service/header-service.service.ts
+++ Angular-Dynamic-Header-With-Url/src/app/service/header-service.service.ts
@@ -1,21 +1,49 @@
 import { Injectable } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeaderServiceService {
+  currentRoute: string = '';
+  constructor(private router: Router) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+      }
+    });
+  }
+  // constructor(private router: Router) {
+  //   router.events.pipe(
+  //     filter(event => event instanceof NavigationEnd)
+  //   ).subscribe((event:NavigationEnd) => {
+  //     this.currentRoute = event.url;
+  //   });
+  // }
   // showFlag = true;
-
-  shouldShowFlag(url: string): boolean {
-    // check if the URL matches a specific pattern where the flag should be hidden
-    console.log(url);
-    if (url.includes('http://localhost:56370/about')) {
-      console.log('hide flag');
-      console.log('hide flag');
-      return false;
+  //list of url
+  urlList = [
+    'about'
+  ];
+  checkUrl():boolean{
+    for (let i = 0; i < this.urlList.length; i++) {
+      if (this.currentRoute.includes(this.urlList[i])) {
+        return false;
+      }
     }
-    console.log('show flag');
-    // otherwise, show the flag
     return true;
   }
+  // shouldShowFlag(url: string): boolean {
+  //   // check if the URL matches a specific pattern where the flag should be hidden
+  //   console.log(url);
+  //   if (url.includes('http://localhost:56370/about')) {
+  //     console.log('hide flag');
+  //     console.log('hide flag');
+  //     return false;
+  //   }
+  //   console.log('show flag');
+  //   // otherwise, show the flag
+  //   return true;
+  // }
 }
