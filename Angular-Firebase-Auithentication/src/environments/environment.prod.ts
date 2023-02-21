d61661bd635442cbfea6a382f748aeae6e45290c
Jaffar Abbas <gamaportal8@gmail.com>
2023-02-22 00:48:04 +0500

    2-22-2023

--- Angular-Firebase-Auithentication/src/environments/environment.prod.ts
+++ Angular-Firebase-Auithentication/src/environments/environment.prod.ts
@@ -1,11 +1,17 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyCMvJg_wk7Kp4GQVr_dTR9y6z-pMa60tjs",
-    authDomain: "angular-whatsapp-clone-1399.firebaseapp.com",
-    projectId: "angular-whatsapp-clone-1399",
-    storageBucket: "angular-whatsapp-clone-1399.appspot.com",
-    messagingSenderId: "937253371067",
-    appId: "1:937253371067:web:cca7073b3d71454e5f7d9e"
+    // apiKey: "AIzaSyCMvJg_wk7Kp4GQVr_dTR9y6z-pMa60tjs",
+    // authDomain: "angular-whatsapp-clone-1399.firebaseapp.com",
+    // projectId: "angular-whatsapp-clone-1399",
+    // storageBucket: "angular-whatsapp-clone-1399.appspot.com",
+    // messagingSenderId: "937253371067",
+    // appId: "1:937253371067:web:cca7073b3d71454e5f7d9e"
+    apiKey: "AIzaSyBAfKvgvlW-ibETGAPCghaZ34e9WkuZLAE",
+    authDomain: "trackitfypapp.firebaseapp.com",
+    projectId: "trackitfypapp",
+    storageBucket: "trackitfypapp.appspot.com",
+    messagingSenderId: "71846593434",
+    appId: "1:71846593434:web:536e8a44740f5e8ae13023"
   }
 };
