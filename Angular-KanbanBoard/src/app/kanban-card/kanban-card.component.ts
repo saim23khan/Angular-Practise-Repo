5078fb4f50ef4eae044fcb4e716ea2c29003f9c5
Jaffar Abbas <gamaportal8@gmail.com>
2023-04-08 19:07:57 +0500

    Commited File : kanban-card.component.ts at 4/8/2023 7:07:57 PM

---
+++ Angular-KanbanBoard/src/app/kanban-card/kanban-card.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban-card',
+  templateUrl: './kanban-card.component.html',
+  styleUrls: ['./kanban-card.component.scss']
+})
+export class KanbanCardComponent {
+  @Input() cardItem!: string;
+}
