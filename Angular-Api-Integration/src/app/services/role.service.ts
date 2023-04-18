b34f28a24f44629a4858547999e1913ad03cbfce
Jaffar Abbas <gamaportal8@gmail.com>
2023-04-18 15:10:35 +0500

    Commited File : role.service.ts at 4/18/2023 3:10:34 PM

---
+++ Angular-Api-Integration/src/app/services/role.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {ApiService} from "./api.service";
+import {Role} from "../components/table/table.component";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleService extends ApiService<Role> {
+  constructor(http: HttpClient) {
+    super(http);
+    this.apiUrl = 'https://localhost:7059/api/';
+    this.apiController = 'role';
+  }
+}
