9950fbddff799827ac1e29cbd82fee4a3370f585
Jaffar Abbas <gamaportal8@gmail.com>
2023-04-22 21:27:05 +0500

    Commited File : app.module.ts at 4/22/2023 9:27:05 PM

--- Angular-Prime-Ng/src/app/app.module.ts
+++ Angular-Prime-Ng/src/app/app.module.ts
@@ -4,23 +4,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimngModuleModule } from './primng-module/primng-module.module';
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { DragdropComponent } from './components/dragdrop/dragdrop.component';
 import { DragDropModule } from 'primeng/dragdrop';
+import { ChipsComponent } from './components/chips/chips.component';
+import {AutoCompleteModule} from "primeng/autocomplete";
+import {DropdownModule} from "primeng/dropdown";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    DragdropComponent
+    DragdropComponent,
+    ChipsComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     // PrimngModuleModule,
-    DragDropModule
+    DragDropModule,
+    PrimngModuleModule,
+    FormsModule,
+    AutoCompleteModule,
+    DropdownModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    HttpClientModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
