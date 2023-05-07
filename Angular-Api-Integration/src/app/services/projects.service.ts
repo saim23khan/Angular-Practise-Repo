4b6dc202b4dbcb24eb7a1fa315af7a72142c12cb
Jaffar Abbas <gamaportal8@gmail.com>
2023-05-07 13:53:30 +0500

    Commited File : projects.service.ts at 5/7/2023 1:53:30 PM

---
+++ Angular-Api-Integration/src/app/services/projects.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { HttpClient } from '@angular/common/http';
+
+export interface Project {
+  ProjectId: number;
+  ProjectName?: string;
+  ProjectCreatedAt?: string;
+  ProjectEditedAt?: string;
+  ProjectStatus?: boolean;
+  ProjectCreatedBy?: number;
+  KanbanBoards?: any[];
+  ProjectAssigneds?: any[];
+  ProjectCreatedByNavigation?: any;
+  ProjectDetails?: any[];
+  Reports?: any[];
+  TaskAssignings?: any[];
+}
+
+
+export interface ProjectDetails {
+  ProjectDetailsId: number;
+  ProjectId: number;
+  ProjectDetailsDescription?: string;
+  ProjectDetailsRoleId?: number;
+  ProjectDetailsProfileId?: number;
+  ProjectDetailsCoverId?: number;
+  ProjectDetailsCreatedAt?: string;
+  ProjectDetailsEditedAt?: string;
+  ProjectDetailsStatus?: boolean;
+  Project?: Project;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectsService extends ApiService<ProjectDetails> {
+
+  constructor(http: HttpClient) {
+    super(http);
+    this.apiUrl = 'https://localhost:7059/api/';
+    this.apiController = 'project';
+  }
+}
