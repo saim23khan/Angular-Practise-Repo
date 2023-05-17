51414fb6acffec42a051c02538717d05935634d7
Jaffar Abbas <gamaportal8@gmail.com>
2023-05-17 14:25:00 +0500

    Commited File : app.component.ts at 5/17/2023 2:25:00 PM

--- Angular-Firebase-Chat-App/src/app/app.component.ts
+++ Angular-Firebase-Chat-App/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { Firestore, addDoc, collection, collectionData, deleteDoc, doc, updateDoc } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -9,45 +8,4 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'Angular-Firebase-Chat-App';
-  formData!:Observable<any[]>;
-  constructor(private firestore:Firestore){
-    this.getData();
-  }
-
-  addForm(forms: any) {
-    const collectionData = collection(this.firestore, 'forms');
-    addDoc(collectionData, forms.value).then(() => {
-      alert('Form added successfully');
-    }).catch((error) => {
-        alert('Error adding form: ' + error);
-    });
-  }
-
-  getData(){
-    const collectionInstance = collection(this.firestore, 'forms');
-    collectionData(collectionInstance , {idField: 'id'}).subscribe((data:any) => {
-      console.log(data);
-    });
-    this.formData = collectionData(collectionInstance , {idField: 'id'});
-    console.log(this.formData);
-  }
-
-  updateForm(id:any) {
-    const collectionInstance = doc(this.firestore, 'forms',id);
-    const updateData = { name: 'Updated Name' };
-    updateDoc(collectionInstance, updateData).then(() => {
-      alert('Form updated successfully');
-    }).catch((error) => {
-        alert('Error updating form: ' + error);
-    });
-  }
-
-  deleteForm(id:any) {
-    const collectionInstance = doc(this.firestore, 'forms',id);
-    deleteDoc(collectionInstance).then(() => {
-      alert('Form deleted successfully');
-    }).catch((error) => {
-        alert('Error deleting form: ' + error);
-    });
-  }
 }
