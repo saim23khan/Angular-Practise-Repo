72b73bd5449e182287a1cf194beae985e50cbf51
Jaffar Abbas <gamaportal8@gmail.com>
2023-05-24 16:54:52 +0500

    Commited File : app.module.ts at 5/24/2023 4:54:51 PM

--- Angular-Firebase-Chat-App/src/app/app.module.ts
+++ Angular-Firebase-Chat-App/src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppComponent } from './app.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TestComponent } from './components/test/test.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ImageUploaderComponent } from './components/image-uploader/image-uploader.component';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import {FIREBASE_OPTIONS} from "@angular/fire/compat";
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    HomeComponent
+    HomeComponent,
+    ImageUploaderComponent
   ],
   imports: [
     BrowserModule,
@@ -24,9 +28,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
+    // provideStorage(() => getStorage()),
+    AngularFireStorageModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
