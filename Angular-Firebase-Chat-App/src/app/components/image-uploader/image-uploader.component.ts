4ef3401ea08584eeba68a6edcf8a9f19e2de5c83
Jaffar Abbas <gamaportal8@gmail.com>
2023-05-24 22:08:06 +0500

    Commited File : image-uploader.component.ts at 5/24/2023 10:08:05 PM

--- Angular-Firebase-Chat-App/src/app/components/image-uploader/image-uploader.component.ts
+++ Angular-Firebase-Chat-App/src/app/components/image-uploader/image-uploader.component.ts
@@ -3,6 +3,7 @@ import { async } from '@angular/core/testing';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { FirebaseStorage } from '@angular/fire/storage';
 import { FormBuilder, FormGroup } from "@angular/forms";
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-image-uploader',
   templateUrl: './image-uploader.component.html',
@@ -10,9 +11,9 @@ import { FormBuilder, FormGroup } from "@angular/forms";
 })
 export class ImageUploaderComponent implements OnInit {
 
-  imageURL!: string;
+  imageURL: string[] = [];
   uploadForm: FormGroup;
-  constructor(public fb: FormBuilder,private angularFireStorage:AngularFireStorage) {
+  constructor(public fb: FormBuilder,private angularFireStorage:AngularFireStorage,private sanitizer: DomSanitizer) {
     // Reactive Form
     this.uploadForm = this.fb.group({
       avatar: [null],
@@ -22,36 +23,54 @@ export class ImageUploaderComponent implements OnInit {
   ngOnInit(): void { }
 
   // Image Preview
-  showPreview(event:any) {
-    // @ts-ignore
-    const file = (event.target as HTMLInputElement).files[0];
-    this.uploadForm.patchValue({
-      avatar: file
-    });
-    // @ts-ignore
-    this.uploadForm.get('avatar').updateValueAndValidity()
-    // File Preview
-    const reader = new FileReader();
-    reader.onload = () => {
-      this.imageURL = reader.result as string;
-    }
-    reader.readAsDataURL(file)
-  }
-  // Submit Form
-  submit() {
-    console.log(this.uploadForm.value)
-  }
-  // private readonly storage: Storage = inject(Storage);
 
   public url!:string;
   async uploadFile(event:any) {
-    const file = event.target.files[0];
-    if(file){
-      console.log(file);
-      // const filePath = `images/${file.name}`;
-      // const task = await this.angularFireStorage.upload(filePath, file);
-      // const url = await task.ref.getDownloadURL();
-      // this.url = url;
+    // @ts-ignore
+    let file = (event.target as HTMLInputElement).files;
+    let fileData;
+    for (let i = 0; i < file!.length; i++) {
+      // if(file![i]){
+        fileData = file![i];
+        let reader = new FileReader();
+        reader.onload = (fileData) => {
+          this.imageURL[i] = reader.result?.toString()!;
+        }
+        reader.readAsDataURL(fileData);
+        console.log(fileData);
+        // await this.upload(file);
+      // }
     }
+  }
+
+  selectedFiles: File[] = [];
+
+  getFileExtension(fileName: string): string {
+    return fileName.split('.').pop()?.toLowerCase() || '';
+  }
+
+  async onFileSelected(event: any) {
+    this.selectedFiles = event.target.files;
+    for (let i = 0; i < this.selectedFiles.length; i++) {
+      console.log(this.getFileType(this.selectedFiles[i]));
+      // await this.upload(this.selectedFiles[i]);ss
+    }
+  }
+
+  getSafeUrl(file: File): SafeUrl {
+    const url = URL.createObjectURL(file);
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+
+  async upload(file:any){
+    const filePath = `images/${file.name}`;
+    const task = await this.angularFireStorage.upload(filePath, file);
+    const url = await task.ref.getDownloadURL();
+    this.url = url;
+  }
+
+  //write function that return type of file
+  getFileType(file: File): string {
+    return file.type.split('/')[0];
   }
 }
