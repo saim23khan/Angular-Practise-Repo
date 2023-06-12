7bbd445b4239b98e88ec7d63fede06f8b583857e
Jaffar Abbas <gamaportal8@gmail.com>
2023-06-12 09:39:13 +0500

    Commited File : canvas-charts.component.ts at 6/12/2023 9:39:13 AM

---
+++ Angular-Charts-Integration/src/app/components/canvas-charts/canvas-charts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas-charts',
+  templateUrl: './canvas-charts.component.html',
+  styleUrls: ['./canvas-charts.component.scss']
+})
+export class CanvasChartsComponent {
+
+}
