03251d894e44a13ef1ef7183eca6561f9d3ae1b9
Jaffar Abbas <gamaportal8@gmail.com>
2023-06-12 09:39:16 +0500

    Commited File : ng-charts.component.ts at 6/12/2023 9:39:15 AM

---
+++ Angular-Charts-Integration/src/app/components/ng-charts/ng-charts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-charts',
+  templateUrl: './ng-charts.component.html',
+  styleUrls: ['./ng-charts.component.scss']
+})
+export class NgChartsComponent {
+
+}
