109066a413ddcc8d16247d860c46f47704c07bd1
Jaffar Abbas <gamaportal8@gmail.com>
2023-06-12 09:39:07 +0500

    Commited File : apex-charts.component.ts at 6/12/2023 9:39:07 AM

---
+++ Angular-Charts-Integration/src/app/components/apex-charts/apex-charts.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {
+  ApexNonAxisChartSeries,
+  ApexPlotOptions,
+  ApexChart
+} from "ng-apexcharts";
+
+@Component({
+  selector: 'app-apex-charts',
+  templateUrl: './apex-charts.component.html',
+  styleUrls: ['./apex-charts.component.scss']
+})
+export class ApexChartsComponent {
+
+}
