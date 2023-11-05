d8c933d7433d87c6ab16dd4c5264ed6105bb8b46
jaffarabbas <gamaportal8@gmail.com>
2023-11-05 22:07:24 +0500

    Commited File : custom-pipe.pipe.ts at 05/11/2023 10:07:24 pm

---
+++ AngularPipes/src/app/pipes/custom-pipe.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'customPipe'
+})
+export class CustomPipePipe implements PipeTransform {
+
+  transform(value: number, ...args: number[]): any {
+    return value;
+  }
+
+}
